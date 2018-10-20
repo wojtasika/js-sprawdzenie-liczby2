6215f8ad8f12109c1815f80c28d2405e9355b33f
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-10-20 19:54:03 +0200

    nowy

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+function printAb(a,b) {
+    
+    if (a === 20 || b === 20) {
+        console.log('true');
+    } else if ((a + b) <= 20) {
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+}
+
+printAb(12, 2);
+
